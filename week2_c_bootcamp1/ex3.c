a572cbc9d48b6367a6dd7a0e3ff7b6d0cbfe079f
kailinghoo <146651600+kailinghoo@users.noreply.github.com>
2023-10-09 09:56:06 +0000

    ADD bootcamp C

---
+++ week2_c_bootcamp1/ex3.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+    int maxint = int_max;
+    int maxfloat = float_max;
+    int maxdouble = double_max;
+    int maxstring = string_max;
+    printf("%d %d %d %d", maxint maxfloat maxdouble maxstring);
+    return 0;
+}
